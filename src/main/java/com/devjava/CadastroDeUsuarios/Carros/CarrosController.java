8c74dce5f8195947d30a0706e47c8c7cd5e4529c
Filipe borges <filipe_mborges@live.com>
2025-08-14 20:39:35 -0300

    feat: Atualiza o controller com mock de dados

---
+++ src/main/java/com/devjava/CadastroDeUsuarios/Carros/CarrosController.java
@@ -0,0 +1,27 @@
+package com.devjava.CadastroDeUsuarios.Carros;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CarrosController {
+
+
+    //Adicionar usuário  (CREATE)
+
+
+    //Procurar usuário por ID  (CREATE)
+
+
+    //Mostrar Usuários por ID   (READ)
+
+
+    //Alterar dados dos usuários  (UPDATE)
+
+
+    //Deletar usuário    (DELETE)
+
+}
--- src/main/java/com/devjava/CadastroDeUsuarios/Usuario/UsuarioController.java
+++ src/main/java/com/devjava/CadastroDeUsuarios/Usuario/UsuarioController.java
@@ -1,8 +1,6 @@
 package com.devjava.CadastroDeUsuarios.Usuario;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,40 @@ public class UsuarioController {
     @GetMapping("/boasVindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem!";
+    }
+
+    //Adicionar usuário  (CREATE)
+    @PostMapping("/criar")
+    public String criarUsuario(){
+        return "Usuário Criado";
+
+    }
+
+    //Mostra todos usuários (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosUsuaris(){
+        return "Mostrar todos os usuários";
+    }
+
+
+    //Mostrar Usuários por ID   (READ)
+    @GetMapping("/todosID")
+    public String mostrarUsuarioID(){
+        return "Mostrar usuários por ID";
+    }
+
+
+    //Alterar dados dos usuários  (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarUsuarioPorId(){
+        return "Alerar usuário por ID";
+    }
+
+
+    //Deletar usuário    (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletaUsuarioPorId(){
+        return "Usuário deletado por ID";
     }
 
 }
