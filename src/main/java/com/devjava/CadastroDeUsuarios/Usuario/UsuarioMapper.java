3baed96dc98a7323e25754a6444204ee9743cc6f
Filipe borges <filipe_mborges@live.com>
2025-08-22 22:19:05 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no usuárioMapper os caminhos entidade dto

--- src/main/java/com/devjava/CadastroDeUsuarios/Usuario/UsuarioMapper.java
+++ src/main/java/com/devjava/CadastroDeUsuarios/Usuario/UsuarioMapper.java
@@ -1,4 +1,32 @@
 package com.devjava.CadastroDeUsuarios.Usuario;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UsuarioMapper {
+
+
+    public UsuarioModel map(UsuarioDTO usuarioDTO){
+        UsuarioModel usuarioModel = new UsuarioModel();
+        usuarioModel.setId(usuarioDTO.getId());
+        usuarioModel.setNome(usuarioDTO.getNome());
+        usuarioModel.setEmail(usuarioDTO.getEmail());
+        usuarioModel.setIdade(usuarioDTO.getIdade());
+        usuarioModel.setCarros(usuarioDTO.getCarros());
+
+        return usuarioModel;
+    }
+    public UsuarioDTO map(UsuarioModel usuarioModel) {
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setId(usuarioModel.getId());
+        usuarioDTO.setNome(usuarioModel.getNome());
+        usuarioDTO.setEmail(usuarioModel.getEmail());
+        usuarioDTO.setIdade(usuarioModel.getIdade());
+        usuarioDTO.setCarros(usuarioModel.getCarros());
+
+        return usuarioDTO;
+    }
+
+
+
 }
